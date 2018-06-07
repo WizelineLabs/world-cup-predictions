c331ffa3e74f354041639625cd1f705c3ec5d9d4
soleit <mohamed.soleit@wizeline.com>
2018-06-07 16:54:10 -0500

    populate predictions script

---
+++ predict_matches.py
@@ -0,0 +1,74 @@
+import os
+import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "world_cup_predictions.settings")
+
+import django
+django.setup()
+
+from api.models import Team, HistoricalGame, WorldCupGame, User, Vote, Prediction, Group
+from model.worldcup_predictor import predict_group_match, win_knockout_match, fetch_matches, get_defense_capabilities
+from datetime import datetime as dt
+import numpy as np
+import pandas as pd
+
+threshold = '2016-06-14'
+historical_matches = fetch_matches('results.csv', threshold)
+defense = get_defense_capabilities(historical_matches)
+
+group_matches = WorldCupGame.objects.filter(pk__lte=48)
+knockout_matches = WorldCupGame.objects.filter(pk__gt=48)
+
+def populate_predictions(matches):
+  for match in matches:
+    prediction_outcome = predict_group_match(str(match.home_team), str(match.away_team), historical_matches, defense)
+    try:
+      prediction = Prediction.objects.get(pk=match.id)
+      prediction.home_win = prediction_outcome['win']
+      prediction.away_win = prediction_outcome['lose']
+      prediction.draw = prediction_outcome['draw']
+      prediction.save()
+    except:
+      Prediction.objects.create(
+        game_id=match.id,
+        home_win=prediction_outcome['win'],
+        away_win=prediction_outcome['lose'],
+        draw=prediction_outcome['draw']
+      )
+def populate_group_predictions(group_matches):
+  for match in group_matches:
+    prediction_outcome = predict_group_match(str(match.home_team), str(match.away_team), historical_matches, defense)
+    try:
+      prediction = Prediction.objects.get(pk=match.id)
+      prediction.home_win = prediction_outcome['win']
+      prediction.away_win = prediction_outcome['lose']
+      prediction.draw = prediction_outcome['draw']
+      prediction.save()
+    except:
+      Prediction.objects.create(
+        game_id=match.id,
+        home_win=prediction_outcome['win'],
+        away_win=prediction_outcome['lose'],
+        draw=prediction_outcome['draw']
+      )
+
+def populate_knockout_predictions(knockout_matches):
+  for match in knockout_matches:
+    home_win = win_knockout_match(str(match.home_team), str(match.away_team), historical_matches, defense)
+    try:
+      prediction = Prediction.objects.get(pk=match.id)
+      prediction.home_win = home_win
+      prediction.away_win = 1 - home_win
+      prediction.draw = 0
+      prediction.save()
+    except:
+      Prediction.objects.create(
+        game_id=match.id,
+        home_win=home_win,
+        away_win=(1-home_win),
+        draw=0
+      )
+
+
+populate_group_predictions(group_matches)
+populate_knockout_predictions(knockout_matches)
