e28fc8f738fdd61d8073654b3275b24728d7fe1c
soleit <mohamed.soleit@wizeline.com>
2018-06-15 02:17:01 -0500

    change the script to check first for correct score

--- update_users_scores.py
+++ update_users_scores.py
@@ -39,8 +39,11 @@ def evaluate_votes(votes):
 def update_scores(users):
   for user in users:
     correct_votes = user.votes.filter(correct=True).count()
-    user.score = correct_votes * ONE_CORRECT_VOTE_SCORE
-    user.save()
+    correct_score = correct_votes * ONE_CORRECT_VOTE_SCORE
+    if user.score != correct_score:
+      print('score of user %s is not correct, updating' % (user.id))
+      user.score = correct_score
+      user.save()
 
 played_games_votes = Vote.objects.filter(game__date__lte=timezone.now())
 evaluate_votes(played_games_votes)
